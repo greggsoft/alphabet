e305bbe2a8697796fa40b123cd65ee430c0f77b0
Mikhail Menshenin <greggsoft@gmail.com>
2026-04-05 11:32:46 +0500

    add pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   root: 'src',
@@ -7,4 +8,28 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true,
   },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Азбука',
+        short_name: 'Азбука',
+        description: 'Учим буквы русского алфавита',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png}'],
+      },
+    }),
+  ],
 });
